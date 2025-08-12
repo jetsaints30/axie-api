4d208ca2a4b4a7a40e04688ff11657b6d1d2d218
jetsaints30 <128446763+jetsaints30@users.noreply.github.com>
2025-08-13 03:18:39 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
@@ -8,4 +9,25 @@ def home():
 
 @app.get("/axie/{axie_id}")
 def get_axie(axie_id: int):
-    return {"axie_id": axie_id}
+    url = "https://graphql-gateway.axieinfinity.com/graphql"
+    query = """
+    query GetAxie($axieId: ID!) {
+      axie(axieId: $axieId) {
+        id
+        bodyParts {
+          name
+          type
+        }
+      }
+    }
+    """
+    variables = {"axieId": str(axie_id)}
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
+    data = response.json()
+
+    if "errors" in data:
+        return {"error": data["errors"]}
+
+    return data["data"]["axie"]
