31407dc21097beb51065144ed0910723cd9e780d
jetsaints30 <128446763+jetsaints30@users.noreply.github.com>
2025-08-13 03:23:54 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,25 +9,29 @@ def home():
 
 @app.get("/axie/{axie_id}")
 def get_axie(axie_id: int):
-    url = "https://graphql-gateway.axieinfinity.com/graphql"
-    query = """
-    query GetAxie($axieId: ID!) {
-      axie(axieId: $axieId) {
-        id
-        bodyParts {
-          name
-          type
+    try:
+        url = "https://graphql-gateway.axieinfinity.com/graphql"
+        query = """
+        query GetAxie($axieId: ID!) {
+          axie(axieId: $axieId) {
+            id
+            bodyParts {
+              name
+              type
+            }
+          }
         }
-      }
-    }
-    """
-    variables = {"axieId": str(axie_id)}
-    headers = {"Content-Type": "application/json"}
+        """
+        variables = {"axieId": str(axie_id)}
+        headers = {"Content-Type": "application/json"}
 
-    response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
-    data = response.json()
+        response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
+        response.raise_for_status()
+        data = response.json()
 
-    if "errors" in data:
-        return {"error": data["errors"]}
+        if "errors" in data:
+            return {"error": data["errors"]}
 
-    return data["data"]["axie"]
+        return data["data"]["axie"]
+    except Exception as e:
+        return {"error": str(e)}
